5b9b3b65199913093d9284c9b2c9496eab1b0572
Sebastian Orjuela <sebasorjuela@gmail.com>
2024-03-17 18:47:54 -0500

    Final details of this

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from typing import Annotated
-
-import xmltodict
 from fastapi import FastAPI, HTTPException, Depends, Request
 from fastapi.responses import Response
 from sqlalchemy.orm import Session
-
-import models
 from database import SessionLocal, engine
+import xmltodict
+import models
+
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -26,21 +25,12 @@ db_dependency = Annotated[Session, Depends(get_db)]
 @app.get("/questions/all")
 async def get_all_questions(db: db_dependency):
     questions = db.query(models.Question).all()
-    choices = db.query(models.Choice).all()
     response_data = {
         "response": {
             "questions": {
                 "question": [
                     {
-                        "question_text": question.question_text,
-                        "choices": [
-                            {
-                                "choice_text": choice.choice_text,
-                                "is_correct": choice.is_correct
-                            }
-                            for choice in choices
-                            if choice.question_id == question.id
-                        ]
+                        "question_text": question.question_text
                     }
                     for question in questions
                 ]
@@ -56,22 +46,13 @@ async def create_question(question: Request, db: db_dependency):
     body = await question.body()
     try:
         data = xmltodict.parse(body)
-        if 'question' not in data or 'question_text' not in data['question'] or 'choices' not in data['question']:
+        if 'question' not in data or 'question_text' not in data['question'] not in data['question']:
             raise HTTPException(status_code=400, detail="Malformed XML.")
-
         question_text = data['question']['question_text']
-        choices = data['question']['choices']['choice']
         db_question = models.Question(question_text=question_text)
         db.add(db_question)
         db.commit()
         db.refresh(db_question)
-        for choice in choices:
-            choice_text = choice['choice_text']
-            is_correct = bool(choice['is_correct'])
-            db_choice = models.Choice(choice_text=choice_text,
-                                      is_correct=is_correct,
-                                      question_id=db_question.id)
-            db.add(db_choice)
         db.commit()
         response_data = {
             "response": {
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, String, Column, Integer, ForeignKey
+from sqlalchemy import String, Column, Integer
 
 from database import Base
 
@@ -8,12 +8,3 @@ class Question(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     question_text = Column(String, index=True)
-
-
-class Choice(Base):
-    __tablename__ = 'choices'
-
-    id = Column(Integer, primary_key=True, index=True)
-    choice_text = Column(String, index=True)
-    is_correct = Column(Boolean, default=False)
-    question_id = Column(Integer, ForeignKey('questions.id'))
