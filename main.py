ec3866189122cad812ec8a4cbde30fd7daa0de88
Sebastian Orjuela <sebasorjuela@gmail.com>
2024-03-18 18:25:45 -0500

    Final details of backend

--- main.py
+++ main.py
@@ -39,29 +39,3 @@ async def get_all_questions(db: db_dependency):
     }
     xml_response = xmltodict.unparse(response_data)
     return Response(content=xml_response, media_type='application/xml')
-
-
-@app.post("/questions/")
-async def create_question(question: Request, db: db_dependency):
-    body = await question.body()
-    try:
-        data = xmltodict.parse(body)
-        if 'question' not in data or 'question_text' not in data['question'] not in data['question']:
-            raise HTTPException(status_code=400, detail="Malformed XML.")
-        question_text = data['question']['question_text']
-        db_question = models.Question(question_text=question_text)
-        db.add(db_question)
-        db.commit()
-        db.refresh(db_question)
-        db.commit()
-        response_data = {
-            "response": {
-                "message": "XML recibido correctamente.",
-                "data": data
-            }
-        }
-        xml_response = xmltodict.unparse(response_data)
-        return Response(content=xml_response, media_type='application/xml')
-
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
